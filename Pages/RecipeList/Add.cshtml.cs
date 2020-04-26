803d86b4f1a2e432360f3432465f9e39bea9978c
Ashley Fogle <ashley.fogle@outlook.com>
2020-04-26 14:50:37 -0400

    Incorporated Add and Edit functionality

---
+++ Pages/RecipeList/Add.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RecipeAppV3.Models;
+
+namespace RecipeAppV3.Pages.RecipeList
+{
+    public class AddModel : PageModel
+    {
+        private readonly RecipeDbContext _db;
+
+        public AddModel(RecipeDbContext db)
+        {
+            _db = db;
+        }
+        
+        [BindProperty]
+        public Recipe Recipes { get; set; }
+        
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Recipes.AddAsync(Recipes);
+                await _db.SaveChangesAsync(); //pushes changes to the database
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
---
+++ Pages/RecipeList/Edit.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RecipeAppV3.Models;
+
+namespace RecipeAppV3.Pages.RecipeList
+{
+    public class EditModel : PageModel
+    {
+        private readonly RecipeDbContext _db;
+
+        public EditModel(RecipeDbContext db)
+        {
+            _db = db;
+        }
+        [BindProperty]
+        public Recipe Recipes { get; set; }
+        public async Task OnGet(int ID)
+        {
+            Recipes = await _db.Recipes.FindAsync(ID);
+        }
+    }
+}
