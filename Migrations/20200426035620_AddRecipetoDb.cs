0ce6f3ea6b16ffd32210ce536d0687977490f76a
Ashley Fogle <ashley.fogle@outlook.com>
2020-04-26 02:12:53 -0400

    Connected to Database. Created table display for recipes. Added Edit and Delete buttons.

---
+++ Migrations/20200426035620_AddRecipetoDb.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecipeAppV3.Migrations
+{
+    public partial class AddRecipetoDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Recipes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RecipeName = table.Column<string>(nullable: false),
+                    RecipeInstructions = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipes", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Recipes");
+        }
+    }
+}
---
+++ Pages/RecipeList/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RecipeAppV3.Models;
+
+namespace RecipeAppV3.Pages.RecipeList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RecipeDbContext _db;
+
+        public IndexModel(RecipeDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Recipe> Recipes { get; set; }
+        public async Task OnGet()
+        {
+            Recipes = await _db.Recipes.ToListAsync();
+
+        }
+    }
+}
