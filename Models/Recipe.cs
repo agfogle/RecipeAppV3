045a50149b97f36562f469b436e7035ae1676ef9
Ashley Fogle <ashley.fogle@outlook.com>
2020-04-25 22:39:24 -0400

    Created DbContext class to work with database

---
+++ Models/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeAppV3.Models
+{
+    public class Recipe
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string RecipeName { get; set; }
+        [Required]
+        public string RecipeInstructions { get; set; }
+    }
+}
